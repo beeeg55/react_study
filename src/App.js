e81850886b0288e99aaafd9d535aba5fac3b4e27
beeeg55 <beeeg@naver.com>
2023-01-04 22:22:55 +0900

    jsx 문법 공부

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = '김단비';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="react">
+      {/* 주석다는법 */}
+      {name}
     </div>
   );
 }
